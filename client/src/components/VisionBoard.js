7465693697b0080e1ea5b860f0a18faa9b986262
Ateja <atejukas123@gmail.com>
2023-06-13 09:47:05 +0100

    using dropzone

--- client/src/components/VisionBoard.js
+++ client/src/components/VisionBoard.js
@@ -1,16 +1,42 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
+import axios from 'axios';
+import { useDropzone } from 'react-dropzone';
 import "../App.css";
 
+
 export default function VisionBoard() {
+    const [uploadedImages, setUploadedImages] = useState([]);
 
-
-  return (
-    <div>
-      <h1 className="vision">
-        VisionBoard
-      </h1>
-      <div className="try"></div>
-    </div>
-  );
-}
+    const onDrop = async (acceptedFiles) => {
+      const formData = new FormData();
+      formData.append('image', acceptedFiles[0]);
+  
+      try {
+        const response = await axios.post('/api/upload', formData, {
+          headers: { 'Content-Type': 'multipart/form-data' },
+        });
+        const { imageUrl } = response.data;
+        setUploadedImages([...uploadedImages, imageUrl]);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+  
+    const { getRootProps, getInputProps } = useDropzone({ onDrop });
+  
+    return (
+      <div>
+        <div {...getRootProps()}>
+          <input {...getInputProps()} />
+          <p>Drag and drop an image here or click to select an image.</p>
+        </div>
+        <div>
+          {uploadedImages.map((imageUrl) => (
+            <img key={imageUrl} src={imageUrl} alt="Uploaded" />
+          ))}
+        </div>
+      </div>
+    );
+  };
+  
